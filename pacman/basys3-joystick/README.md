b53ca92f44fbe61a4e341acfa78d8a82216ca504
bushraay <97578618+bushraay@users.noreply.github.com>
2022-12-13 10:09:22 -0800

    Add files via upload

---
+++ pacman/basys3-joystick/README.md
@@ -0,0 +1,3 @@
+# Basys3-Joystick-Interfacing-main
+ Basys3 Joystick Test
+ reference: https://github.com/Digilent/Basys3.git
---
+++ pacman/basys3-joystick/test.srcs/sources_1/new/wall_collision.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/13/2022 03:07:46 PM
+// Design Name: 
+// Module Name: wall_collision
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module wall_collision(
+    input clk,
+    input x,
+    input y,
+    input r,
+    output right
+    );
+//    always @ (posedge clk)
+//    begin
+    
+//    end
+endmodule
